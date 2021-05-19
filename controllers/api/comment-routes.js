360779333df8e78825dc75b9491bdb54a299a873
Scott Erwin <scottallenerwin@gmail.com>
2021-05-19 16:35:04 -0400

    created templates for comment post user routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const { Comment } = require("../../models/");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ controllers/api/post-routes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const { Post } = require("../../models/");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
