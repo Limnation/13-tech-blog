53592d389f85a78fde243d8ce6f7974241c70320
Scott Erwin <scottallenerwin@gmail.com>
2021-05-19 20:48:34 -0400

    updated comment post user routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,6 +3,11 @@ const { Comment } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
+  const newComment = await Comment.create({
+    ...req.body,
+    userId: req.session.userId,
+  });
+  res.json(newComment);
   try {
   } catch (err) {
     res.status(500).json(err);
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,13 +3,31 @@ const { Post } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
+  const reqBody = req.body;
   try {
+    const newPost = await Post.create({
+      ...reqBody,
+      userId: req.session.userId,
+    });
+    res.json(newPost);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.put("/:id", withAuth, async (req, res) => {
+  const affRows = [];
+  affRows = await Post.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (affRows > 0) {
+    res.status(200).end();
+  } else {
+    res.status(404).end();
+  }
   try {
   } catch (err) {
     res.status(500).json(err);
@@ -17,6 +35,18 @@ router.put("/:id", withAuth, async (req, res) => {
 });
 
 router.delete("/:id", withAuth, async (req, res) => {
+  const affRows = [];
+  affRows = Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (affRows > 0) {
+    res.status(200).end();
+  } else {
+    res.status(404).end();
+  }
   try {
   } catch (err) {
     res.status(500).json(err);
